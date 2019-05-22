0f23e68f30158f47fa3513748b0a784c549230f2
Brett Jory <bjory7@gmail.com>
2019-05-22 14:48:54 -0700

    routes successfully imported into server; test object displays on root URL

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static(__dirname + '/public/dist/public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+require('./server/routes')(app);
 const port = 8000;
 const server = app.listen(port, () => {
 	console.log('server listening on port', port);
