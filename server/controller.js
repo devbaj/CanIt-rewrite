cf6043d6509a687a4f236d28bb7e3061c5ca2817
Brett Jory <bjory7@gmail.com>
2019-05-22 17:05:21 -0700

    Database connection successful; Users model created but untested; unsure how to proceed re: migrations

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
-const models = require('./models');
+const Models = require('./models');
+const User = Models.User;
 
 module.exports = {
 	index: (req, res) => res.json({message: 'controller is working'})
--- server/models.js
+++ server/models.js
@@ -6,7 +6,64 @@ const sequelize = new Sequelize('can_it', 'root', 'root', {
 });
 
 // Connection test
-// sequelize
-// 	.authenticate()
-// 	.then( () => console.log('database connection successfully established'))
-// 	.catch( err => console.log('Unable to connect to database', err));
+sequelize
+	.authenticate()
+	.then( () => console.log('database connection successfully established'))
+	.catch( err => console.log('Unable to connect to database', err));
+
+const User = sequelize.define('User', {
+	firstName: {
+		type: Sequelize.STRING,
+		field: 'first_name',
+		allowNull: false,
+		validate: {
+			notNull: true,
+			notEmpty: true,
+			len: [2,30]
+		}
+	},
+	lastName: {
+		type: Sequelize.STRING,
+		field: 'last_name',
+		allowNull: false,
+		validate: {
+			notNull: true,
+			notEmpty: true,
+			len: [2,30]
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notNull: true,
+			isEmail: true
+		}
+	},
+	username: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notNull: true,
+			isAlpha: true,
+		}
+	},
+	hashedPW: {
+		type: Sequelize.STRING,
+		field: 'hashed_pw',
+		allowNull: false,
+		validate: {
+			notNull: true
+		}
+	}
+}, {
+	timestamps: true,
+	paranoid: true,
+	updatedAt: 'updated_at',
+	createdAt: 'created_at',
+	deletedAt: 'deleted_at'
+})
+
+module.exports = {
+	User: User
+}
